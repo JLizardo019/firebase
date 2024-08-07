33810f0de43da8955a52acb9dd3962e1484df169
JLizardo019 <jal879@nyu.edu>
2024-08-07 15:50:23 -0400

    updating my code

--- init-firebase.js
+++ init-firebase.js
@@ -2,7 +2,15 @@
  * @TODO replace the firebaseConfig object below with the one
  * provided for you by firebase after creating your project!
  */
-const firebaseConfig = {};
+const firebaseConfig = {
+    apiKey: "AIzaSyA_3YQapj6edWMIfh90c5BFqsbyEbnMh4w",
+    authDomain: "fire-chat-9f737.firebaseapp.com",
+    databaseURL: "https://fire-chat-9f737-default-rtdb.firebaseio.com",
+    projectId: "fire-chat-9f737",
+    storageBucket: "fire-chat-9f737.appspot.com",
+    messagingSenderId: "505558171021",
+    appId: "1:505558171021:web:03757f199be6ca735291c4"
+  };
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
